c4edaf4028be105870b76eb82820bcb29ab063b3
Yun <yuniya.pa98@gmail.com>
2022-07-21 11:27:59 -0400

    DB info added

--- dbinfo.js
+++ dbinfo.js
@@ -85,7 +85,7 @@ const Restaurant = [
     type: 'Dinner',
     about:
       'Team-up with your favorite Super Heroes after a day of adventures and defeat the most formidable foe of all—your empty stomach. You’re dining at Cafe 4 when in walk Captain America, Spider-Man, Wolverine, Storm and Rogue. This is no ordinary dinner. This is an experience that no true believer can miss.',
-    reservation: 'neded',
+    reservation: 'Call for reservation',
     image:
       'https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/ioa-marvel-character-dinner-spiderman-family-a.jpg'
   },
@@ -94,6 +94,7 @@ const Restaurant = [
     type: 'Quick Service',
     about:
       'Tell your hunger, "It is clobberin time!" Atop of the Baxter Building, the elite super group the Fantastic Four meets in their prominent headquarters and high-tech laboratory. And on the infamous buildings ground floor, you will find a convenient Italian eatery serving pizza, spaghetti and meatballs, fettuccini, meatball subs, and chicken Caesar salads.',
+    reservation: 'not needed',
     image:
       'https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/ioa-cafe-4-universal-inside-a.jpg'
   },
@@ -102,6 +103,7 @@ const Restaurant = [
     type: 'Quick Service',
     about:
       'Avengers Assemble! Superhuman hunger does not stand a chance at this eye-catching eatery. You have found the place where the heroes hang out, and you will see them all over the walls of this star-spangled diner. Enjoy a classic and delicious menu of cheeseburgers, chicken sandwiches, chicken fingers, crispy chicken salads, and more.',
+    reservation: 'not needed',
     image:
       'https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/ioa-captain-america-diner-ceiling-a.jpg'
   },
@@ -110,6 +112,7 @@ const Restaurant = [
     type: 'Ice cream, milkshakes',
     about:
       'Even Super Heroes need a snack break—especially Iceman, whose frozen facade stands solid outside this cool walk-up counter. Here, Häagen-Dazs® ice cream is proudly served up just the way you like. Scoop up a sundae, milkshake, ice cream float, or freshly made waffle cone before heading on to your next adventure.',
+    reservation: 'not needed',
     image:
       'https://www.universalorlando.com/webdata/k2/en/us/files/Images/gds/ioa-chill-ice-cream-universal-iceman-sign-a.jpg'
   },
