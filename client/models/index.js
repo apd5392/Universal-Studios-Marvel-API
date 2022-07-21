13d34cee84c0abad49e1b72d3ab6910692345213
Yun <yuniya.pa98@gmail.com>
2022-07-21 14:55:52 -0400

    all seeds are working

--- client/models/index.js
+++ client/models/index.js
@@ -1,9 +1,12 @@
 const { model } = require('mongoose')
 const ParkSchema = require('./park')
 const RestaurantSchema = require('./restaurant')
+const RideSchema = require('./ride')
+const ShopSchema = require('./shops')
 const Ride = model('Ride', RideSchema)
 const Park = model('Park', ParkSchema)
 const Shop = model('Shop', ShopSchema)
+const Restaurant = model('Restaurant', RestaurantSchema)
 
 module.exports = {
   Ride,
