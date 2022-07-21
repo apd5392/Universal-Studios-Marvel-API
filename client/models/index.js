548f8a37f44e5bdceabc57caf02b4e9f7ae0a532
sahidankhan <sahidankhan7991@gmail.com>
2022-07-21 13:21:54 -0500

    commit

--- client/models/index.js
+++ client/models/index.js
@@ -1,17 +1,17 @@
 const {model} = require('mongoose')
 const ParkSchema = require('./park')
-const ResturantSchema = require('./resturant')
+const RestaurantSchema = require('./resturant')
 const RideSchema = require('./ride')
 const ShopSchema = require('./shops')
 
 const Ride = model("Ride", RideSchema)
 const Park = model('Park', ParkSchema)
-const Resturant = model('Resurant', ResturantSchema)
+const Restaurant = model('Resurant', ResturantSchema)
 const Shop = model('Shop', ShopSchema)
 
 module.exports={
     Ride,
     Park,
-    Resturant,
+    Restaurant,
     Shop
 }
